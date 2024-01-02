e017face4bdbdc06a7e173f01ec4cc62dc082317
Lucas <lucassarachu7@gmail.com>
2024-01-02 11:26:46 +0100

    Creando componente add-character

---
+++ src/app/dbz/components/list-dbz/list-dbz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-dbz',
+  templateUrl: './list-dbz.component.html',
+  styleUrl: './list-dbz.component.css'
+})
+export class ListDbzComponent {
+
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListDbzComponent } from './components/list-dbz/list-dbz.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListDbzComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
