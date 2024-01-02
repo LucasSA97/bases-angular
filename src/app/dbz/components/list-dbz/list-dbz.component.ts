60a78e28b1a590f12a707730709d6404f3f9d7d6
Lucas <lucassarachu7@gmail.com>
2024-01-02 11:47:13 +0100

    Recibiendo del componente padre con @Input

--- src/app/dbz/components/list-dbz/list-dbz.component.ts
+++ src/app/dbz/components/list-dbz/list-dbz.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-list-dbz',
+  selector: 'list-dbz',
   templateUrl: './list-dbz.component.html',
   styleUrl: './list-dbz.component.css'
 })
 export class ListDbzComponent {
 
+  @Input()
+
+  public characterList: Character[] = [
+    {
+      name: 'trunks',
+      power: 200
+    }
+  ]
 }
